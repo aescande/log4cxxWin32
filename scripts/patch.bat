5b09f85390f42e009d9ec73f0212fe93db611df6
Escande Adrien <adrien.escande@gmail.com>
2016-05-25 18:11:02 +0900

    change directory name from patch to scripts

--- scripts/patch.bat
+++ scripts/patch.bat
@@ -1,6 +1,11 @@
 ECHO OFF
+ECHO "Start patching log4cxx"
 sed -i "/#include <vector>/ a #include<iterator>" ..\log4cxx\src\main\cpp\stringhelper.cpp
-sed -i "/namespace log4cxx/ i #define DELETED_CTORS(T) \\\n        T(const T&) = delete;\\\n        T& operator=(const T&) = delete;\n\n#define DEFAULTED_AND_DELETED_CTORS(T) \\\n        T() = default;\\\n        T(const T&) = delete;\\\n        T& operator=(const T&) = delete;\n" ..\log4cxx\src\main\include\log4cxx\helpers\objectimpl.h
+IF DEFINED APPVEYOR (
+  sed -i "/namespace log4cxx/ i #define DELETED_CTORS(T) \\\\\n        T(const T&) = delete;\\\\\n        T& operator=(const T&) = delete;\n\n#define DEFAULTED_AND_DELETED_CTORS(T) \\\\\n        T() = default;\\\\\n        T(const T&) = delete;\\\\\n        T& operator=(const T&) = delete;\n" ..\log4cxx\src\main\include\log4cxx\helpers\objectimpl.h
+) ELSE (
+  sed -i "/namespace log4cxx/ i #define DELETED_CTORS(T) \\\n        T(const T&) = delete;\\\n        T& operator=(const T&) = delete;\n\n#define DEFAULTED_AND_DELETED_CTORS(T) \\\n        T() = default;\\\n        T(const T&) = delete;\\\n        T& operator=(const T&) = delete;\n" ..\log4cxx\src\main\include\log4cxx\helpers\objectimpl.h
+)
 sed -i "/END_LOG4CXX_CAST_MAP()/ a \  DEFAULTED_AND_DELETED_CTORS(PatternConverter)" ..\log4cxx\src\main\include\log4cxx\pattern\patternconverter.h
 sed -i "/virtual ~RollingPolicyBase();/ i \          DELETED_CTORS(RollingPolicyBase)" ..\log4cxx\src\main\include\log4cxx\rolling\RollingPolicyBase.h
 sed -i "/virtual ~TriggeringPolicy();/ i \             DEFAULTED_AND_DELETED_CTORS(TriggeringPolicy)" ..\log4cxx\src\main\include\log4cxx\rolling\TriggeringPolicy.h
@@ -9,3 +14,4 @@ sed -i "/virtual ~Layout();/ i \                DEFAULTED_AND_DELETED_CTORS(Layo
 sed -i -e "s/defined(_MSC_VER) \&\&/defined(_MSC_VER) \&\& _MSC_VER < 1600 \&\&/" ..\log4cxx\src\main\include\log4cxx\log4cxx.h
 REM removing temporary files created by sed
 DEL sed*
+ECHO "Patching done"
